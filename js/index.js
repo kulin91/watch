afecf12f573a830ded69b9a4db3a76ee4d2be727
Alexandr Kulinkovich <alexandr_kulinkovich@icloud.com>
2021-04-23 01:36:16 +0300

    add france watch

--- js/index.js
+++ js/index.js
@@ -1,21 +1,30 @@
-const timeNumbersElement = document.getElementById("belarus_time-numbers");
-const secondHandElement = document.getElementById("belarus_second_hand");
-const minuteHandElement = document.getElementById("belarus_minute_hand");
-const hourseHandElement = document.getElementById("belarus_hourse_hand");
+const timeNumbersElementForBelarus = document.getElementById("belarus_time-numbers");
+const secondHandElementForBelarus = document.getElementById("belarus_second_hand");
+const minuteHandElementForBelarus = document.getElementById("belarus_minute_hand");
+const hourseHandElementForBelarus = document.getElementById("belarus_hourse_hand");
 
+const timeNumbersElementForFrance = document.getElementById("france_time-numbers");
+const secondHandElementForFrance = document.getElementById("france_second_hand");
+const minuteHandElementForFrance = document.getElementById("france_minute_hand");
+const hourseHandElementForFrance = document.getElementById("france_hourse_hand");
 
-function timeNow() {
-  const currentTime = luxon.DateTime.now();
-  const time = currentTime.toLocaleString(luxon.DateTime.TIME_24_WITH_SECONDS);
-  return time;
+const regionFrance = 'UTC+2';
+const regionBelarus = 'UTC+3';
+const regionChina
+
+function timeNow(region) {
+  const currentTime = luxon.DateTime.now().setZone(region);
+  const result = currentTime.toLocaleString(luxon.DateTime.TIME_24_WITH_SECONDS);
+  return result;
 };
 
 
-timeNumbersElement.innerHTML = timeNow();
+timeNumbersElementForBelarus.innerHTML = timeNow(regionBelarus);
+timeNumbersElementForFrance.innerHTML = timeNow(regionFrance);
 
 
-function createSecondDeg () {
-  const timesToClockHand = luxon.DateTime.now();
+function createSecondDeg(region) {
+  const timesToClockHand = luxon.DateTime.now().setZone(region);
   const secondsPosition = timesToClockHand.second;
   const degSeconds = 6;
   const result = secondsPosition * degSeconds;
@@ -26,8 +35,8 @@ function createSecondDeg () {
 }
 
 
-function createMinuteDeg() {
-  const timesToClockHand = luxon.DateTime.now();
+function createMinuteDeg(region) {
+  const timesToClockHand = luxon.DateTime.now().setZone(region);
   const minutePosition = timesToClockHand.minute;
   const degMinute = 6;
   const result = minutePosition * degMinute;
@@ -38,8 +47,8 @@ function createMinuteDeg() {
 }
 
 
-function createHourseDeg() {
-  const timesToClockHand = luxon.DateTime.now();
+function createHourseDeg(region) {
+  const timesToClockHand = luxon.DateTime.now().setZone(region);
   const hoursePosition = timesToClockHand.toFormat('h');
   const degHourse = 30;
   const result = hoursePosition * degHourse;
@@ -51,14 +60,24 @@ function createHourseDeg() {
 
 
 setInterval(() => {
-  timeNumbersElement.innerHTML = timeNow();
-  const angleSecond = createSecondDeg();
-  secondHandElement.style.transform = `rotate(${angleSecond}deg)`;
-  const angleMinute = createMinuteDeg();
-  minuteHandElement.style.transform = `rotate(${angleMinute}deg)`;
-  const angleHourse = createHourseDeg();
-  hourseHandElement.style.transform = `rotate(${angleHourse}deg)`;
+  timeNumbersElementForBelarus.innerHTML = timeNow(regionBelarus);
+  const angleSecondForBelarus = createSecondDeg(regionBelarus);
+  secondHandElementForBelarus.style.transform = `rotate(${angleSecondForBelarus}deg)`;
+  const angleMinuteForBelarus = createMinuteDeg(regionBelarus);
+  minuteHandElementForBelarus.style.transform = `rotate(${angleMinuteForBelarus}deg)`;
+  const angleHourseForBelarus = createHourseDeg(regionBelarus);
+  hourseHandElementForBelarus.style.transform = `rotate(${angleHourseForBelarus}deg)`;
+
+  timeNumbersElementForFrance.innerHTML = timeNow(regionFrance);
+  const angleSecondForFrance = createSecondDeg(regionFrance);
+  secondHandElementForFrance.style.transform = `rotate(${angleSecondForFrance}deg)`;
+  const angleMinuteForFrance = createMinuteDeg(regionFrance);
+  minuteHandElementForFrance.style.transform = `rotate(${angleMinuteForFrance}deg)`;
+  const angleHourseForFrance = createHourseDeg(regionFrance);
+  hourseHandElementForFrance.style.transform = `rotate(${angleHourseForFrance}deg)`;
 }, 1000);
+
+
 
 
 
